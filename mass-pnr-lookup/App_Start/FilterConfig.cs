0debe936351e64561c802b05f55e5c6a75046f5f
beemen <beshara_beemen@hotmail.com>
2016-03-04 11:55:59 +0100

    Initial ASP.NET web application

---
+++ mass-pnr-lookup/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace mass_pnr_lookup
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ mass-pnr-lookup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mass_pnr_lookup.MvcApplication" Language="C#" %>
