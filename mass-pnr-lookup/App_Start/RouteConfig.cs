339f198b5147a7d95e5c991a92effb7f12703a9a
beemen <beshara_beemen@hotmail.com>
2016-03-09 12:16:06 +0100

    Minor fixesfor queues

--- mass-pnr-lookup/App_Start/RouteConfig.cs
+++ mass-pnr-lookup/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace mass_pnr_lookup
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Files", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
