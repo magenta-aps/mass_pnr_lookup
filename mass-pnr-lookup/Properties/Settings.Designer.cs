d60078655a2efcb39f8f7792c24b809ddedff162
beemen <beshara_beemen@hotmail.com>
2016-03-14 15:55:09 +0100

    Configurable connection string

--- mass-pnr-lookup/Properties/Settings.Designer.cs
+++ mass-pnr-lookup/Properties/Settings.Designer.cs
@@ -26,8 +26,6 @@ namespace mass_pnr_lookup.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=Localhost\\SQLEXPRESS;Initial Catalog=mass_pnr_lookup;Integrated Secur" +
-            "ity=True")]
         public string MassPnrLookupConnectionString {
             get {
                 return ((string)(this["MassPnrLookupConnectionString"]));
