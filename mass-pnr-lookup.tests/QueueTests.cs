b687692aeb96a98fe2d655fca79a783a1fe56ce5
beemen <beshara_beemen@hotmail.com>
2016-03-16 11:07:04 +0100

    Added QueueTests class

---
+++ mass-pnr-lookup.tests/QueueTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mass_pnr_lookup.Queues;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mass_pnr_lookup.tests
+{
+    [TestClass]
+    class QueueTests
+    {
+        [TestMethod]
+        public void RunQueues()
+        {
+            CprBroker.Engine.BrokerContext.Initialize(CprBroker.Utilities.Constants.BaseApplicationToken.ToString(), "Testing developer");
+
+            CprBroker.Engine.Queues.Queue.GetQueues<ExtractionQueue>().Single().RunAll();
+            CprBroker.Engine.Queues.Queue.GetQueues<SearchQueue>().Single().RunAll();
+            CprBroker.Engine.Queues.Queue.GetQueues<OutputGenerationQueue>().Single().RunAll();
+            CprBroker.Engine.Queues.Queue.GetQueues<UserNotificationQueue>().Single().RunAll();
+        }
+    }
+}
