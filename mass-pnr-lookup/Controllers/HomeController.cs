ef27f1695d5f532f6520b9d86d9265d8881b8dfb
beemen <beshara_beemen@hotmail.com>
2016-03-17 13:44:03 +0100

    Removed home index page

--- mass-pnr-lookup/Controllers/HomeController.cs
+++ mass-pnr-lookup/Controllers/HomeController.cs
@@ -8,11 +8,6 @@ namespace mass_pnr_lookup.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "";
