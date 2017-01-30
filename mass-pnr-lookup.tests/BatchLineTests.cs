a210881bf23584c842c13674a638c03b2302560a
beemen <beshara_beemen@hotmail.com>
2017-01-30 11:34:21 +0100

    Unit tests for BatchLine.FillFrom(..)

---
+++ mass-pnr-lookup.tests/BatchLineTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using mass_pnr_lookup.Models;
+
+namespace mass_pnr_lookup.tests
+{
+    namespace BatchLineTests
+    {
+        [TestClass]
+        public class FillFrom
+        {
+            [TestMethod]
+            public void FillFrom_First_Found()
+            {
+                FillFrom_Success(0);
+            }
+
+            [TestMethod]
+            public void FillFrom_Second_Found()
+            {
+                FillFrom_Success(1);
+            }
+
+            [TestMethod]
+            public void FillFrom_Third_Found()
+            {
+                FillFrom_Success(2);
+            }
+
+            public void FillFrom_Success(int index)
+            {
+                var factory = new SoegListOutputTypeFactory();
+                var names = new string[] { "first1 middle1 last1", "first1 middle1 middle12 last1", "first2 middle2 last2" };
+                var expected = names.First();
+                
+                var names2 = names.Skip(1).ToList();
+                names2.Insert(index, expected);
+
+                var sr = factory.Create(names2.ToArray());
+                var bl = new BatchLine() { Name = expected };
+                var success = bl.FillFrom(sr);
+                Assert.IsTrue(success);
+                Assert.AreEqual(expected, bl.MatchedName);
+            }
+
+            [TestMethod]
+            public void FillFrom_UnmatchedName_False()
+            {
+                var factory = new SoegListOutputTypeFactory();
+                var names = new string[] { "first1 middle1 last1", "first1 middle1 middle12 last1", "first2 middle2 last2" };
+                var expected = "first1 middle1 middle2 last1";
+
+                var sr = factory.Create(names);
+                var bl = new BatchLine() { Name = expected };
+                var success = bl.FillFrom(sr);
+                Assert.IsFalse(success);
+
+            }
+        }
+    }
+}
---
+++ mass-pnr-lookup.tests/SoegListOutputTypeFactory.cs
@@ -0,0 +1,48 @@
+﻿using CprBroker.Schemas.Part;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mass_pnr_lookup.tests
+{
+    class SoegListOutputTypeFactory
+    {
+        public SoegListOutputType Create(params string[] names)
+        {
+            var ret = new SoegListOutputType();
+
+            var objects = names.Select(
+                n =>
+                {
+                    var ooo = new LaesResultatType()
+                    {
+                        Item = new FiltreretOejebliksbilledeType()
+                        {
+                            AttributListe = new AttributListeType()
+                            {
+                                Egenskab = new EgenskabType[] {
+                                    new EgenskabType() {
+                                        NavnStruktur = new NavnStrukturType() {
+                                            PersonNameStructure = new PersonNameStructureType(n.Split(' ')) }
+                                        } },
+                                RegisterOplysning = new RegisterOplysningType[]
+                                {
+                                    new RegisterOplysningType() {
+                                        Item = new CprBorgerType() {
+                                        PersonCivilRegistrationIdentifier = "0123456789"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    };
+                    return ooo;
+                });
+
+            ret.LaesResultat = objects.ToArray();
+            return ret;
+        }
+    }
+}
