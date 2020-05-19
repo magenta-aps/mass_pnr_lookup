344c9c1ff769f1ec8c6c2f7dbfa34e032b8c8549
heiniovason <heini.ovason@gmail.com>
2020-05-19 09:39:30 +0200

    Classname changed to filenames

--- mass-pnr-lookup/CprBrokerLogger.cs
+++ mass-pnr-lookup/CprBrokerLogger.cs
@@ -6,7 +6,7 @@ using CprBroker.Engine.local;
 
 namespace mass_pnr_lookup
 {
-    public class LogInCprBroker
+    public class CprBrokerLogger
     {
         public static void LogSuccess(string msg)
         {
