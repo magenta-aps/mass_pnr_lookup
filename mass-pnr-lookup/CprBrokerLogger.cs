0270a0a75e462ef041a78afb5e271d860bb4d218
heiniovason <heini.ovason@gmail.com>
2020-05-19 09:02:02 +0200

    filename changed.

---
+++ mass-pnr-lookup/CprBrokerLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CprBroker.Engine.local;
+
+namespace mass_pnr_lookup
+{
+    public class LogInCprBroker
+    {
+        public static void LogSuccess(string msg)
+        {
+            Admin.LogSuccess(msg);
+        }
+
+        public static void LogError(string msg)
+        {
+            Admin.LogError(msg);
+        }
+
+        public static void LogException(Exception ex, string msg)
+        {
+            Admin.LogException(ex, msg);
+        }
+    }
+}
